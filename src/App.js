419280f5550773fbbe94fb70a96559b900c39947
Vanshika Ahuja <vanshikaahuja012@gmail.com>
2026-06-12 14:29:53 +0530

    feat: configure Tailwind CSS and create UI components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
-import Generate from "./pages/Generate";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/generate" element={<Generate />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 export default App;
